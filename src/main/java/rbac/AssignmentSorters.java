881f81f3ad5679c373942d2c7bd101b82c116f81
Кормилицын Илья Сергеевич <kormilitsyn.ilya@mail.ru>
2026-02-17 10:47:39 +0700

    2.4. Sorting

---
+++ src/main/java/rbac/AssignmentSorters.java
@@ -0,0 +1,17 @@
+package rbac;
+
+import java.util.Comparator;
+
+public class AssignmentSorters {
+    public static Comparator<RoleAssignment> byUsername() {
+        return Comparator.comparing(assignment -> assignment.user().username());
+    }
+
+    public static Comparator<RoleAssignment> byRoleName() {
+        return Comparator.comparing(assignment -> assignment.role().name());
+    }
+
+    public static Comparator<RoleAssignment> byAssignmentDate() {
+        return Comparator.comparing(assignment -> assignment.metadata().assignedAt());
+    }
+}
